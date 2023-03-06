d3821e5044dd58b83bb282e5d1dd8c9f17d0ba9d
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-03-06 18:43:55 +0100

    refactor(_document): transform into functional component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,16 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-
-import { AppConfig } from '@/utils/AppConfig';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
-class MyDocument extends Document {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head>
-          <meta charSet="UTF-8" key="charset" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1"
-            key="viewport"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const MyDocument = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 export default MyDocument;
